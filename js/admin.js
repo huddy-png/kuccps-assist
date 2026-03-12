25b96b9984784caffa2c6d77fc2fc6366039bfa2
ZACKGEE <zacharyomedo@gmail.com>
2026-03-13 02:47:17 +0300

    add animated scroll button

--- js/admin.js
+++ js/admin.js
@@ -1072,3 +1072,19 @@ searchInput.addEventListener("input", () => {
   adminMsg.textContent = "✅ Admin access confirmed.";
   fetchBookings();
 })();
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollBtn.classList.add("show");
+  } else {
+    scrollBtn.classList.remove("show");
+  }
+});
+
+scrollBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
